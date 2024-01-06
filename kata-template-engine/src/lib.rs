6fd5c095298684a08275725d516f02af7d30c9b4
magecnion <magecnion@gmail.com>
2024-01-06 17:02:31 +0100

    add custom errors

--- kata-template-engine/src/lib.rs
+++ kata-template-engine/src/lib.rs
@@ -1,13 +1,16 @@
 use std::collections::HashMap;
-use std::error::Error;
+use std::fmt;
 
-fn apply_template(text: &str, dictionary: &str) -> String {
-    let dictionary = build_dictionary(dictionary).unwrap();
+fn apply_template(text: &str, dictionary: &str) -> Result<String, TemplateError> {
+    if text.is_empty() {
+        return Err(TemplateError::EmptyFile);
+    }
+    let dictionary = build_dictionary(dictionary)?;
     let mut result = text.to_string();
     for (key, value) in dictionary {
         result = replace(&result, (&key, &value));
     }
-    result
+    Ok(result)
 }
 
 fn replace(text: &str, word_and_value: (&str, &str)) -> String {
@@ -17,18 +20,33 @@ fn replace(text: &str, word_and_value: (&str, &str)) -> String {
 type Dictionary = HashMap<String, String>;
 
 fn build_dictionary(json: &str) -> Result<Dictionary, TemplateError> {
-    serde_json::from_str::<Dictionary>(json).map_err(TemplateError::JsonError)
+    let dicttionary = serde_json::from_str::<Dictionary>(json).map_err(TemplateError::JsonError)?;
+    if dicttionary.is_empty() {
+        return Err(TemplateError::EmptyDictionary);
+    }
+    Ok(dicttionary)
 }
 
 #[derive(Debug)]
 enum TemplateError {
     JsonError(serde_json::Error),
+    EmptyDictionary,
+    EmptyFile,
+}
+
+impl fmt::Display for TemplateError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            TemplateError::JsonError(ref err) => write!(f, "JSON error: {}", err),
+            TemplateError::EmptyDictionary => write!(f, "Empty dictionary is not allowed"),
+            TemplateError::EmptyFile => write!(f, "Empty file is not allowed"),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use serde_json::Error as SerdeJsonError;
 
     #[test]
     fn replace_one_word() {
@@ -52,7 +70,7 @@ mod tests {
     fn given_a_text_apply_template() {
         let dictionary = r#"{"name": "John", "greet": "Hello"}"#;
         let text = "${greet}, ${name}";
-        let result = apply_template(text, dictionary);
+        let result = apply_template(text, dictionary).unwrap();
         assert_eq!(result, "Hello, John");
     }
 
@@ -60,7 +78,7 @@ mod tests {
     fn given_a_text_apply_template_when_not_all_variables_exist() {
         let dictionary = r#"{"greet": "Hello"}"#;
         let text = "${greet}, ${name}";
-        let result = apply_template(text, dictionary);
+        let result = apply_template(text, dictionary).unwrap();
         assert_eq!(result, "Hello, ${name}");
     }
 
@@ -76,7 +94,26 @@ mod tests {
     fn given_an_invalid_json_string_it_raises_an_error() {
         let json = r#"{"name" "John", "greet": "Hello"}"#;
         let result = build_dictionary(json);
-        assert!(result.is_err());
-        assert_eq!(result.unwrap_err(), "");
+        assert_eq!(
+            result.unwrap_err().to_string(),
+            "JSON error: expected `:` at line 1 column 9"
+        );
+    }
+
+    #[test]
+    fn given_an_empty_json_it_raises_an_error() {
+        let json = r#"{}"#;
+        let result = apply_template("a", json);
+        assert_eq!(
+            result.unwrap_err().to_string(),
+            "Empty dictionary is not allowed"
+        );
+    }
+
+    #[test]
+    fn given_an_empty_file_it_raises_an_error() {
+        let json = r#"{"name": "John", "greet": "Hello"}"#;
+        let result = apply_template("", json);
+        assert_eq!(result.unwrap_err().to_string(), "Empty file is not allowed");
     }
 }
