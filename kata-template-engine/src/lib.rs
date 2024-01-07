ed5cc54b487e8bcc42de28f2aaf55734339f3c64
magecnion <magecnion@gmail.com>
2024-01-07 10:52:43 +0100

    add warnings

--- kata-template-engine/src/lib.rs
+++ kata-template-engine/src/lib.rs
@@ -1,42 +1,74 @@
+use regex::Regex;
 use std::collections::HashMap;
 use std::fmt;
 
 pub fn parse_template_from_json(
     template_text: &str,
     json_variables: &str,
-) -> Result<String, TemplateError> {
-    if template_text.is_empty() {
-        return Err(TemplateError::EmptyFile);
-    }
+) -> Result<ParsedTemplate, TemplateError> {
+    check_text_not_empty(template_text)?;
     let dictionary = build_dictionary(json_variables)?;
     parse_template(template_text, dictionary)
+}
+
+fn check_dictionary_not_empty(dictionary: &HashMap<String, String>) -> Result<(), TemplateError> {
+    if dictionary.is_empty() {
+        return Err(TemplateError::EmptyDictionary);
+    }
+    Ok(())
+}
+
+fn check_text_not_empty(text: &str) -> Result<(), TemplateError> {
+    if text.is_empty() {
+        return Err(TemplateError::EmptyFile);
+    }
+    Ok(())
+}
+
+#[derive(Debug)]
+pub struct ParsedTemplate {
+    pub text: String,
+    pub warnings: Vec<String>,
 }
 
 pub fn parse_template(
     template_text: &str,
     variables: HashMap<String, String>,
-) -> Result<String, TemplateError> {
-    if template_text.is_empty() {
-        return Err(TemplateError::EmptyFile);
-    }
-    let mut parsed_template = template_text.to_string();
+) -> Result<ParsedTemplate, TemplateError> {
+    check_text_not_empty(template_text)?;
+    check_dictionary_not_empty(&variables)?;
+    let mut warnings = Vec::new();
+    let mut parsed_template_text = template_text.to_string();
     for (variable, value) in variables {
-        parsed_template = replace(&parsed_template, (&variable, &value));
+        parsed_template_text = parsed_template_text.replace(&format!("${{{}}}", variable), &value);
+        if !parsed_template_text.contains(&value) {
+            warnings.push(format!("Variable {} not found", variable));
+        }
     }
-    Ok(parsed_template)
+    let not_replaced_variables_warnings =
+        get_warnings_for_not_replaced_variables(parsed_template_text.clone());
+    warnings.extend(not_replaced_variables_warnings);
+    Ok(ParsedTemplate {
+        text: parsed_template_text,
+        warnings,
+    })
 }
 
-fn replace(template_text: &str, word_and_value: (&str, &str)) -> String {
-    template_text.replace(&format!("${{{}}}", word_and_value.0), word_and_value.1)
+fn get_warnings_for_not_replaced_variables(parsed_template_text: String) -> Vec<String> {
+    let mut warnings = Vec::new();
+    let re = Regex::new(r"\$\{(\w+)\}").unwrap();
+    for captures in re.captures_iter(&parsed_template_text) {
+        let word = &captures[1];
+        warnings.push(format!("Variable {} not replaced", word));
+    }
+    warnings
 }
 
 type Dictionary = HashMap<String, String>;
 
 fn build_dictionary(json: &str) -> Result<Dictionary, TemplateError> {
     let dicttionary = serde_json::from_str::<Dictionary>(json).map_err(TemplateError::JsonError)?;
-    if dicttionary.is_empty() {
-        return Err(TemplateError::EmptyDictionary);
-    }
+    check_dictionary_not_empty(&dicttionary)?;
     Ok(dicttionary)
 }
 
@@ -60,24 +92,6 @@ impl fmt::Display for TemplateError {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn replace_one_word() {
-        let words_replaced = replace("${greet}", ("greet", "Hello"));
-        assert_eq!(words_replaced, "Hello");
-    }
-
-    #[test]
-    fn replace_with_wrong_format_does_not_replace() {
-        let words_replaced = replace("${greet", ("greet", "Hello"));
-        assert_eq!(words_replaced, "${greet");
-    }
-
-    #[test]
-    fn replace_same_word_two_times() {
-        let words_replaced = replace("${greet}${greet}", ("greet", "Hello"));
-        assert_eq!(words_replaced, "HelloHello");
-    }
 
     #[test]
     fn given_a_valid_json_string_i_can_create_a_dictionary() {
--- kata-template-engine/tests/sociable_tests.rs
+++ kata-template-engine/tests/sociable_tests.rs
@@ -1,11 +1,13 @@
-use kata_template_engine::parse_template_from_json;
+use std::collections::HashMap;
+
+use kata_template_engine::{parse_template, parse_template_from_json};
 
 #[test]
 fn given_a_text_apply_template_from_json() {
     let variables = r#"{"name": "John", "greet": "Hello"}"#;
     let text = "${greet}, ${name}";
     let parsed_template = parse_template_from_json(text, variables).unwrap();
-    assert_eq!(parsed_template, "Hello, John");
+    assert_eq!(parsed_template.text, "Hello, John");
 }
 
 #[test]
@@ -13,13 +15,22 @@ fn given_a_text_apply_template_from_json_when_not_all_variables_exist() {
     let variables = r#"{"greet": "Hello"}"#;
     let text = "${greet}, ${name}";
     let parsed_template = parse_template_from_json(text, variables).unwrap();
-    assert_eq!(parsed_template, "Hello, ${name}");
+    assert_eq!(parsed_template.text, "Hello, ${name}");
 }
 
 #[test]
 fn given_an_empty_json_it_raises_an_error() {
     let variables = r#"{}"#;
     let parsed_template = parse_template_from_json("a", variables);
+    assert_eq!(
+        parsed_template.unwrap_err().to_string(),
+        "Empty dictionary is not allowed"
+    );
+}
+
+#[test]
+fn given_an_empty_dictionary_it_raises_an_error() {
+    let parsed_template = parse_template("a", HashMap::new());
     assert_eq!(
         parsed_template.unwrap_err().to_string(),
         "Empty dictionary is not allowed"
@@ -40,5 +51,8 @@ fn given_an_empty_file_it_raises_an_error() {
 fn given_a_file_where_there_are_no_variables_that_matches_it_returns_the_same_text_and_warnings() {
     let variables = r#"{"name": "John", "greet": "Hello"}"#;
     let parsed_template = parse_template_from_json("Hello, ${surname}", variables).unwrap();
-    assert_eq!(parsed_template, "Hello, John");
+    assert_eq!(parsed_template.text, "Hello, ${surname}");
+    assert_eq!(parsed_template.warnings.len(), 2);
+    assert_eq!(parsed_template.warnings[0], "Variable name not found");
+    assert_eq!(parsed_template.warnings[1], "Variable surname not replaced");
 }
